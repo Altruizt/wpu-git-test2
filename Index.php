f19b81f796513a2fd658606e59d3b4ce934ca6d1
Altruizt2605 <gianginanjar1996@gmail.com>
2021-02-09 16:49:23 +0700

    Menambahkan Palagraf

--- Index.php
+++ Index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, maiores nostrum numquam ipsam tenetur cupiditate culpa vel molestiae eaque id, necessitatibus quia ut deleniti quo tempore. Ratione laudantium voluptates dolorum.</p>
 </body>
 </html>
